4cefd07de104b3c2e24cd9c6532568c9db1e6c42
BattleUSAGI <531744708@qq.com>
2021-05-16 23:45:03 +0800

    更新各种地块prefab

---
+++ Assets/____My Aseets/Scripts/BasicGround.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+
+public class BasicGround : MonoBehaviour
+{
+    public Transform[] points;
+
+    void Awake()
+    {
+        points = new Transform[transform.childCount]; // 有多少个子物体，points数组就有多少个transform
+
+
+
+        for (int i = 0; i < points.Length; i++) //当i小于数组的长度时，i++
+        {
+            points[i] = transform.GetChild(i); //transform数组point的第i个transform，等于该脚本附加的物件的第i个子物件的transform
+        }
+
+
+
+
+
+
+
+
+
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
---
+++ Assets/____My Aseets/Scripts/Enemy_Test.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy_Test : MonoBehaviour
+{
+    public float speed = 10f;
+
+    public BasicGround basicGround;
+
+    private Transform target;
+    private int wavePointIndex = 0;
+
+
+    void Start()
+    {
+        //if (GameObject.Find("WayPoints_2"))
+        //{
+        //    Debug.Log("find");
+            
+        //}
+
+        //basicGround = GameObject.Find("WayPoints_2").GetComponent<BasicGround>();
+        target = basicGround.points[0];
+
+
+        //target = GameObject.Find("WayPoints_2").
+
+        //target = BasicGround.points[0];//前进目标 = 第X个transform点
+    }
+
+
+    void Update()
+    {
+        Vector3 dir = target.position - transform.position;
+        transform.Translate(dir.normalized * speed * Time.deltaTime, Space.World);
+
+        if(Vector3.Distance(transform.position, target.position) <= 0.02f)
+        {
+            GetNextWayPonint();
+        }
+
+
+    }
+
+
+    void GetNextWayPonint()
+    {
+        if(wavePointIndex >= basicGround.points.Length - 1)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+
+
+        wavePointIndex++ ;
+        target = basicGround.points[wavePointIndex];
+
+
+    }
+
+
+
+
+
+
+
+}
